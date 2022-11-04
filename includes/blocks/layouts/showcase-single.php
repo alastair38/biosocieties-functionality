739388430620e5195d91e821d068154ea45dcea5
alastair38 <alastair@alastaircox.com>
2022-11-04 13:11:59 +0000

    Reduced image width

--- includes/blocks/layouts/showcase-single.php
+++ includes/blocks/layouts/showcase-single.php
@@ -38,8 +38,8 @@ if( !empty($block['backgroundColor']) ) {
 ?>
 
 <section id="<?php echo $id;?>" class="<?php echo esc_attr($className); ?> my-6 lg:my-16" >
-<div class="flex flex-col lg:flex-row items-center gap-2 lg:gap-12 mx-auto w-11/12 lg:w-4/5 <?php echo esc_attr($className); ?>" >
-<figure class="w-full flex-1"><?php echo get_the_post_thumbnail($content->ID,'full');?></figure>
+<div class="flex flex-col lg:flex-row items-center gap-2 lg:gap-0 mx-auto w-11/12 lg:w-4/5 <?php echo esc_attr($className); ?>" >
+<figure class="w-full flex-1"><?php echo get_the_post_thumbnail($content->ID,'full', array( 'class' => 'w-2/3' ));?></figure>
 
 <div class="w-full lg:w-4/5 space-y-2 lg:space-y-6 p-6 self-center z-10">
 <h1 class="font-black has-large-font-size">
